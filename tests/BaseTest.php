36a881695ccc72e9f93c4999c02c1c6252aa087f
Rocco Howard <rocco@hnh.digital>
2018-01-29 21:32:57 +0000

    Updated approach to testing; Added regions/$id test

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,6 +2,7 @@
 
 namespace HnhDigital\LinodeApi\Tests;
 
+use HnhDigital\LinodeApi\Foundation\Auth;
 use InterNations\Component\HttpMock\PHPUnit\HttpMockTrait;
 use PHPUnit\Framework\TestCase;
 
@@ -27,5 +28,31 @@ abstract class BaseTest extends TestCase
     public function tearDown()
     {
         $this->tearDownHttpMock();
+    }
+
+    /**
+     * Mock a GET request.
+     */
+    protected function mockRequest($method, $path, $data)
+    {
+        // Encode response.
+        $encoded_data = json_encode($data);
+
+        // Mock the API endpoint and result.
+        $this->http->mock
+            ->when()
+                ->methodIs($method)
+                ->pathIs($path)
+            ->then()
+                ->body($encoded_data)
+            ->end();
+
+        $this->http->setUp();        
+
+        // Set the Linode API to this local server.
+        Auth::setBaseEndpoint('http://localhost:8082/');
+
+        // Ensure the mock server responds with the same data.
+        $this->assertSame($encoded_data, file_get_contents('http://localhost:8082'.$path));
     }
 }
---
+++ tests/RegionsEndpointTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace HnhDigital\LinodeApi\Tests;
+
+use HnhDigital\LinodeApi\Region;
+use HnhDigital\LinodeApi\Regions;
+
+class RegionsEndpointTest extends BaseTest
+{
+    /**
+     * Test GET /regions.
+     *
+     * @return void
+     */
+    public function testGetRegions()
+    {
+        // Regions data.
+        $data = [
+            'pages'   => 1,
+            'results' => 1,
+            'data'    => [
+                [
+                    'id'      => 'us-east-1a',
+                    'country' => 'US',
+                ],
+            ],
+            'page' => 1,
+        ];
+
+        // Setup test server at path and with response data.
+        $this->mockRequest('GET', '/regions?page=1', $data);
+
+        // Create a new regions endpoint.
+        $result = (new Regions())->search()->all();
+
+        // Create the same object that it should return.
+        $region = new Region('us-east-1a', array_get($data, 'data.0'));
+
+        // Compare.
+        $this->assertEquals($region, array_get($result, 0));
+    }
+
+    /**
+     * Test GET region/$region_id.
+     *
+     * @return void
+     */
+    public function testGetRegion()
+    {
+        // Regions data.
+        $data = [
+            'id'      => 'us-east-1a',
+            'country' => 'US',
+        ];
+
+        // Setup test server at path and with response data.
+        $this->mockRequest('GET', '/regions/us-east-1a', $data);
+    }
+}
