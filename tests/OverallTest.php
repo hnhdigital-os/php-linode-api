46295d9cf317cccb3113626dc1425e9a5aaedd9f
Rocco Howard <rocco@hnh.digital>
2018-01-30 13:40:15 +0000

    Check that setting the auth token works

--- tests/OverallTest.php
+++ tests/OverallTest.php
@@ -2,12 +2,18 @@
 
 namespace HnhDigital\LinodeApi\Tests;
 
+use HnhDigital\LinodeApi\Profile;
 use HnhDigital\LinodeApi\StackScript;
 use HnhDigital\LinodeApi\Foundation\Auth;
 
 class OverallTest extends BaseTest
 {
 
+    /**
+     * Test setting, getting attributes.
+     *
+     * @return void
+     */
     public function testAttributes()
     {
         $data = [
@@ -29,5 +35,25 @@ class OverallTest extends BaseTest
 
         // Get the original attributes.        
         $this->assertEquals($data, $stack_script->getOriginalAttributes());
+    }
+
+    /**
+     * Test setting the token, and checking that it is sent with the request.
+     *
+     * @return void
+     */
+    public function testToken()
+    {
+        // Setup test server at path and with response data.
+        $this->mockGetRequest('/profile', []);
+
+        Profile::token('123456789');
+
+        // Get the profile data from the endpoint.
+        $profile = new Profile(true);
+
+        $request = $this->http->requests->last();
+
+        $this->assertTrue($request->getHeaders()->get('authorization')->hasValue('Bearer 123456789'));
     }
 }
